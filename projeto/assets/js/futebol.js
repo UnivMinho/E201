533a710193fc6d045cac399800df1b4f096e8a37
Amadeu2001 <a97589@alunos.uminho.pt>
2023-04-29 12:34:01 +0100

    futebol e reservas

--- projeto/assets/js/futebol.js
+++ projeto/assets/js/futebol.js
@@ -42,4 +42,44 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+
+
+
+
+   //funcao para confirmar reserva
+   const celulas = document.querySelectorAll('td, th');
+
+   for (let i = 0; i < celulas.length; i++) {
+   celulas[i].addEventListener('click', function(event) {
+       if (event.target.nodeName === 'TD' || event.target.nodeName === 'TH') {
+       const celulaClicada = event.target;
+       const indiceCelula = [...celulaClicada.parentNode.children].indexOf(celulaClicada);
+       const cabecalhoCorrespondente = document.querySelector(`th:nth-child(${indiceCelula + 1})`);
+       console.log(`Clicou em ${celulaClicada.textContent} na coluna "${cabecalhoCorrespondente.textContent}"`);
+
+       // Exibe um popup de confirmação
+       const confirmarReserva = window.confirm(`Você deseja reservar o campo para ${celulaClicada.textContent} às ${cabecalhoCorrespondente.textContent}?`);
+
+       if (confirmarReserva) {
+           // Código para reservar a hora selecionada
+           celulaClicada.style.visibility = 'hidden';
+           celulaClicada.style.position = 'absolute';
+           celulaClicada.style.width = `${celulaClicada.offsetWidth}px`;
+           console.log(`Hora ${cabecalhoCorrespondente.textContent} reservada com sucesso!`);
+       } else {
+           // Código para cancelar a reserva
+           console.log(`Reserva cancelada.`);
+       }
+       }
+   });
+   }
+
+
+
+
+    
+    
+
+
 });
